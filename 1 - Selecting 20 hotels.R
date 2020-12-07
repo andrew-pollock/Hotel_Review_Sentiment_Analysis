86a89c9dd1e227e8c4273d44732cd066e3632a4b
Andrew <apollock92@hotmail.com>
2020-12-07 00:08:10 +0000

    Selecting the 20 hotels I'll use

---
+++ 1 - Selecting 20 hotels.R
@@ -0,0 +1,40 @@
+
+## Creating a dataset with only 20 hotels
+
+library(readxl)
+library(dplyr)
+hotel_reviews <- read_excel("data/raw/hotel_reviews.xls")
+
+str(hotel_reviews)
+
+# Lets start by cleaning the variable names
+names(hotel_reviews) <- c("ID", "Hotel_Name", "Location", "Review_Date", "Review")
+
+
+## How many hotels does each location have with at least 100 reviews?
+hotel_reviews %>% group_by(Hotel_Name) %>% mutate(num_reviews = n()) %>% 
+  filter(num_reviews >= 100) %>% group_by(Location) %>% select(Location, Hotel_Name) %>% 
+  distinct() %>% summarise(num_hotels = n()) %>% arrange(-num_hotels)
+
+## Patong and Kata Beach are the two locations with the most hotels with at least 100 reviews
+## A small number of hotels from Patong have more than 100 reviews
+## To ensure a fair comparison, I'll select 10 hotels from each location with exactly 100 reviews
+
+filtered_hotels <- hotel_reviews %>% filter(Location %in% c("Patong", "Kata Beach")) %>% 
+                      group_by(Hotel_Name) %>% mutate(num_reviews = n()) %>% 
+                      filter(num_reviews == 100) %>% select(-num_reviews)
+
+
+## I'll randomly select 10 hotels from each location
+## Setting a seed ensures that this process is replicatable
+set.seed(101)
+
+# Create a vector of the 20 hotels I've randomly selected
+selected_hotels <- filtered_hotels %>% group_by(Location) %>% select(Location, Hotel_Name) %>% 
+                      distinct() %>% sample_n(size = 10, replace = FALSE) %>% ungroup() %>% select(Hotel_Name)
+
+# Select all reviews for those 20 hotels
+selected_reviews <- filtered_hotels %>% filter(Hotel_Name %in% selected_hotels$Hotel_Name)
+
+# Save these reviews down as a .csv
+write.csv(selected_reviews, file = "data/processed/selected_reviews.csv", row.names = FALSE)
